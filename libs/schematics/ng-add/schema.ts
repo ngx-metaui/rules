50ba74c50e7adc9845f8679b0989d37f10468526
fkolar <frantisek.kolar@hawaia.com>
2018-10-20 16:58:52 +0200

    feat(Schematics): Added ng-add schematics support

---
+++ libs/schematics/ng-add/schema.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright Frank Kolar and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+export interface Schema {
+
+  /** Name of the project to target. */
+  project: string;
+
+  /** Dont add package.json dependencies. */
+  skipDependencies: boolean;
+
+  /** Dont run npm install. */
+  skipNpmInstall: boolean;
+
+
+  /** Dont add anything to angular.json script section. */
+  skipScripts: boolean;
+
+
+  /** Dont add anything to angular.json style section. */
+  skipStyles: boolean;
+
+  /** Root path to the selected project  - a place where template files will be copied */
+  path: string;
+
+
+}
--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -10,6 +10,8 @@ fi
 
 rm -Rf ./dist/libs
 
+
+
 if [ ${args[0]} == "prod" ]; then
    echo "################ Building @ngx-meta/rules ################"
    ng build rules --prod
@@ -18,6 +20,32 @@ else
    echo "################ Building @ngx-meta/rules ################"
       ng build rules
 fi
+
+if [ ${args[0]} == "link" ]; then
+   echo "################ Updating package.json with version ################"
+   NEW_VERSION=$(node -p "require('./package.json').version")
+   echo "Updating packages.json under dist/libs/rules with version ${NEW_VERSION}"
+
+   cd ./dist/
+   perl -p -i -e "s/VERSION_PLACEHOLDER/${NEW_VERSION}/g" $(grep -ril VERSION_PLACEHOLDER .) < /dev/null 2> /dev/null
+
+   cd ..
+
+
+fi
+
+# Copy resources
+echo "Building resources"
+rm -Rf ./dist/libs/rules/lib/resources && cp -R libs/rules/src/lib/resources ./dist/libs/rules/lib/resources && ./node_modules/.bin/scss-bundle -e  ./dist/libs/rules/lib/resources/styles/aribaui.scss -d ./dist/libs/rules/lib/resources/styles/aribaui.css
+cp -R libs/rules/src/lib/metaui/core/oss ./dist/libs/rules/lib/metaui/core/oss
+
+
+# Build schematics
+echo "Building schematics"
+npm run build:schematics
+
+
+
 
 
 
